f68bca95b1d3e86b88cf33f46ae4f67fcd38083d
Christopher Smith <christopher.smith@orasi.com>
2018-05-29 13:33:19 -0400

    Play around with xls, csv data files

---
+++ src/main/java/com/trainingserver/CSVBuilder.java
@@ -0,0 +1,30 @@
+package com.trainingserver;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CSVBuilder {
+
+    public static void main(String[] args) throws IOException {
+        writeCSV("Test", 6);
+    }
+
+    private static void writeCSV(String scenario, int numResults) throws IOException {
+        String path = "C:\\Users\\christopher.smith\\repos\\Demo-Java-Automation\\src\\main\\resources\\excelsheets\\trainingServer\\" + scenario + ".csv";
+        File file = new File(path);
+        FileWriter writer = new FileWriter(file);
+        // Write header
+        writer.write(scenario + ",xpath,value\n");
+        // For each node within a given range, write an xpath and regex
+        for (int i = 1; i <= numResults; i++) {
+            writer.write(",Envelope/Body/" + decapitalize(scenario) + "Response/language[" + i + "]/language_id,\\d+\r\n");
+            writer.write(",Envelope/Body/" + decapitalize(scenario) + "Response/language[" + i + "]/name,[A-Za-z]+\\s*\r\n");
+        }
+        writer.close();
+    }
+
+    private static String decapitalize(String string) {
+        return string.substring(0, 1).toLowerCase() + string.substring(1);
+    }
+}
--- src/test/java/com/trainingserver/TestTrainingServer.java
+++ src/test/java/com/trainingserver/TestTrainingServer.java
@@ -4,38 +4,76 @@ import org.testng.annotations.Test;
 
 import com.orasi.utils.Constants;
 import com.orasi.utils.TestReporter;
-import com.trainingserver.operations.GetActiveCustomers;
-import com.trainingserver.operations.GetActorsByFirstName;
-import com.trainingserver.operations.GetActorsById;
+import com.trainingserver.operations.GetAllActors;
+import com.trainingserver.operations.GetAllLanguages;
 
 public class TestTrainingServer {
 
     private String excelFileLocation = Constants.EXCEL_SHEETS + "/trainingServer";
 
+    // @Test
+    // public void getActiveCustomers() {
+    // GetActiveCustomers getCustomers = new GetActiveCustomers();
+    // getCustomers.sendRequest();
+    // TestReporter.logAPI(getCustomers.getNumberOfResults() != 0, "Results were returned (" + getCustomers.getNumberOfResults() + ")", getCustomers);
+    // TestReporter.assertTrue(getCustomers.validateResponse(excelFileLocation + "/GetActiveCustomersResponse.xls", "getActiveCustomers"), "Validate Response");
+    // }
+    //
+    // @Test
+    // public void getActorsByFirstName() {
+    // GetActorsByFirstName getActors = new GetActorsByFirstName();
+    // getActors.setActorFirst_nAme("Joe");
+    // getActors.sendRequest();
+    // String lName = getActors.getResponseNodeValueByXPath("/Envelope/Body/getActorsByFirstNameResponse/actor/last_name");
+    // TestReporter.logAPI(getActors.getNumberOfResults() != 0, "Results were returned (" + getActors.getNumberOfResults() + ")", getActors);
+    // TestReporter.assertTrue(lName.equals("SWANK"), "First actor LName in response == SWANK");
+    // }
+    //
+    // @Test
+    // public void getActorsById() {
+    // GetActorsById getActors = new GetActorsById();
+    // getActors.setActorId("1");
+    // getActors.sendRequest();
+    // int numResults = getActors.getNumberOfResults();
+    // TestReporter.logAPI(numResults != 0, "Results were returned (" + numResults + ")", getActors);
+    // TestReporter.assertTrue(getActors.validateResponse(excelFileLocation + "/GetActorsByIdResponse.csv", "getActorsById"), "Valid GetActorsById Response");
+    // }
+    //
+    // @Test
+    // public void getAddressByCity() {
+    // GetAddressByCity getAddress = new GetAddressByCity();
+    // getAddress.setCityName("Lethbridge");
+    // getAddress.sendRequest();
+    // int numResults = getAddress.getNumberOfResults();
+    // TestReporter.logAPI(numResults != 0, "Results were returned (" + numResults + ")", getAddress);
+    // TestReporter.assertTrue(getAddress.validateResponse(excelFileLocation + "/GetAddressByCity.csv", "getAddressByCity"), "Valid GetAddressByCity Response");
+    // }
+    //
+    // @Test
+    // public void getLanguageById() {
+    // GetLanguageById getLanguage = new GetLanguageById();
+    // getLanguage.setLanguageId("1");
+    // getLanguage.sendRequest();
+    // int numResults = getLanguage.getNumberOfResults();
+    // TestReporter.logAPI(numResults != 0, "Results were returned (" + numResults + ")", getLanguage);
+    // TestReporter.assertTrue(getLanguage.validateResponse(excelFileLocation + "/GetLanguageById.csv", "GetLanguageById"), "Valid GetLanguageById Response");
+    // }
+
     @Test
-    public void getActiveCustomers() {
-        GetActiveCustomers getCustomers = new GetActiveCustomers();
-        getCustomers.sendRequest();
-        TestReporter.logAPI(getCustomers.getNumberOfResults() != 0, "Results were returned (" + getCustomers.getNumberOfResults() + ")", getCustomers);
-        TestReporter.assertTrue(getCustomers.validateResponse(excelFileLocation + "/GetActiveCustomersResponse.xls", "getActiveCustomers"), "Validate Response");
+    public void getAllLanguages() {
+        GetAllLanguages getLanguages = new GetAllLanguages();
+        getLanguages.sendRequest();
+        int numResults = getLanguages.getNumberOfResults();
+        // TestReporter.logAPI(numResults != 0, "Results were returned (" + numResults + ")", getLanguages);
+        TestReporter.assertTrue(getLanguages.validateResponse(excelFileLocation + "/GetAllLanguages.csv", "GetAllLanguages"), "Valid GetAllLanguages Response");
     }
 
     @Test
-    public void getActorsByFirstName() {
-        GetActorsByFirstName getActors = new GetActorsByFirstName();
-        getActors.setActorFirst_nAme("Joe");
-        getActors.sendRequest();
-        String lName = getActors.getResponseNodeValueByXPath("/Envelope/Body/getActorsByFirstNameResponse/actor/last_name");
-        TestReporter.logAPI(getActors.getNumberOfResults() != 0, "Results were returned (" + getActors.getNumberOfResults() + ")", getActors);
-        TestReporter.assertTrue(lName.equals("SWANK"), "First actor LName in response == SWANK");
-    }
-
-    @Test
-    public void getActorsById() {
-        GetActorsById getActors = new GetActorsById();
-        getActors.setActorId("1");
+    public void getAllActors() {
+        GetAllActors getActors = new GetAllActors();
         getActors.sendRequest();
         int numResults = getActors.getNumberOfResults();
         TestReporter.logAPI(numResults != 0, "Results were returned (" + numResults + ")", getActors);
+        TestReporter.assertTrue(getActors.validateResponse(excelFileLocation + "/GetAllActors.csv", "GetAllActors"), "Valid GetAllActors Response");
     }
 }
